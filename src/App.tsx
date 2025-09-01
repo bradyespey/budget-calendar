d297766b310e1c7424763138a5ac168051e12e0b
Brady <baespey@gmail.com>
2025-09-01 15:40:21 -0500

    refactor: Simplify TransactionsPage layout by removing unnecessary Layout component wrapping; enhance Navbar to handle Transactions click event

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
             path="/transactions"
             element={
               <RequireAuth>
-                <Layout><TransactionsPage /></Layout>
+                <TransactionsPage />
               </RequireAuth>
             }
           />
